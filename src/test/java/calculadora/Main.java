f40c9770eef7d28268799d09d299c79e2cccd6a4
Peilei Ye Jin <peilei.ye280@comunidadunit.net>
2025-02-19 10:46:42 +0100

    javadoc actualizado, version final para entrega

--- src/test/java/calculadora/Main.java
+++ src/test/java/calculadora/Main.java
@@ -123,6 +123,7 @@ class Main {
         assertEquals(-7.5, resta.restarAcumulado(2.5));
         assertEquals(-12.6,resta.restarAcumulado(5.1));
     }
+    
    /** Tests para la clase Cociente */
 
     @Test
@@ -163,31 +164,4 @@ class Main {
         assertThrows(IllegalArgumentException.class, () -> cociente.raiz(-16));
     }
 
-
-    /** Tests para la clase Resta */
-
-    @Test
-    void restarDosEnteros() {
-        assertEquals(5, resta.restar(10, 5));
-        assertEquals(8, resta.restar(23, 15));
-    }
-
-    @Test
-    void restarDosReales() {
-        assertEquals(6.3, resta.restar(10.5, 4.2), 0.0001);
-        assertEquals(5.31, resta.restar(15.31, 10.00), 0.0001);
-    }
-
-    @Test
-    void restarTresReales() {
-        assertEquals(5.2, resta.restar(20.5, 10.1, 5.2), 0.0001);
-        assertEquals(33.32, resta.restar(48.67, 10.13, 5.22), 0.0001);
-    }
-
-    @Test
-    void restarAcumulado() {
-        assertEquals(-5.0, resta.restarAcumulado(5.0));
-        assertEquals(-7.5, resta.restarAcumulado(2.5));
-        assertEquals(-12.6,resta.restarAcumulado(5.1));
-    }
 }
